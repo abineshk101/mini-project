1e0879bd48a5ea1a24b672116202e1e0f22df6d0
sheikmohamed17 <alameen232003@gmail.com>
2023-11-28 13:58:13 +0530

    change branch

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-
+import Apidata from './sampleapi';
 import React from 'react';
+import CreateGroup from './group_create/create_group';
 const router = createBrowserRouter([
   {
     path: "/sample",
     element: <Sample />,
   },
+  {
+    path:"/creategroup",
+    element:<CreateGroup />
+  }
 ]);
 
 function App() {
   return(
 
     <React.StrictMode>
+      <Apidata />
     <RouterProvider router={router} />
     </React.StrictMode>
   )
--- src/group_create/create_group.js
+++ src/group_create/create_group.js
@@ -1,9 +1,15 @@
 import React from "react";
 function CreateGroup()
 {
+    const alertdata =()=>
+    {
+        alert('hi')
+    }
     return(
+     
         <>
-        
+        <h4>Group Create</h4>
+        GName<input type="text" name="GroupName" onKeyUp={()=>alertdata()} />
         </>
     )
 }
