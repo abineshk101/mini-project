2dc42a6bdc40c00ac1caed6128ec20b3f18badf1
sangee-21 <sangeethakesavan76@gmail.com>
2023-12-19 11:58:00 +0530

    miniproject

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import React from 'react';
+import UserIndividualDetailes from './Detailes/user_individual_detailes'
 const router = createBrowserRouter([
   {
     path: "/sample",
     element: <Sample />,
   },
+  {
+    path:"/",
+    element:<UserIndividualDetailes/>
+  }
 ]);
 
 function App() {
---
+++ src/Detailes/user_individual_detailes.js
@@ -0,0 +1,43 @@
+import React from "react";
+import axios from 'axios';
+import { useState } from "react";
+import { useEffect } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './style.css'
+// import { useSelector,useDispatch } from "react-redux";
+
+
+function UserIndividualDetailes() {
+  // const individual=useSelector((state)=>state.user.individual)
+  // const dispatch=useDispatch()
+  // useEffect(() => {
+  //   axios.get("https://9e4a60c7-d991-48dc-a0a2-fba826b0bf94.mock.pstmn.io/groupdetailes")
+  //     .then(res => dispatch(individualData(res.data)))
+  // }, [])
+  return (
+    <>
+    <div className="box">
+    <div class="full">
+      <Card class="card text-white bg-dark mb-3" style={{ width: '14rem' }}>
+      <Card.Img class="image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt7C1jVOcBY3LpyT3GJcdQHAYyBuW6Js6h3w&usqp=CAU" />
+      <Card.Body class="body">
+        <Card.Title>USER NAME</Card.Title>
+        <Card.Text>
+          user email
+        </Card.Text>
+        <Card.Text>
+          Phone number
+        </Card.Text>
+        <Card.Text>
+          Amount paid
+        </Card.Text>
+        <Button variant="primary">Pay</Button>
+      </Card.Body>
+    </Card>
+    </div>
+    </div>
+    </>
+  )
+}
+export default UserIndividualDetailes
