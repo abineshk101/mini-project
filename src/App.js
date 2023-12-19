5000b6e3bd68836dd56eb89490d6db528c11fddb
muruges-wari <gayathrikarthi102@gmail.com>
2023-12-19 11:27:48 +0530

    mini project

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-
+import UserIndividualDetailes from './Detailes/user_individual_detailes'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 const router = createBrowserRouter([
   {
     path: "/sample",
     element: <Sample />,
   },
+  {
+    path:"/",
+    element:<UserIndividualDetailes/>
+  }
 ]);
 
 function App() {
