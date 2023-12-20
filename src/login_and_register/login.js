1b80c4caa571b97c311626b2e4e6d0700e925f2f
Abinesh K <abineshk101@gmail.com>
2023-12-20 14:41:25 +0530

    before redux in login

--- src/login_and_register/login.js
+++ src/login_and_register/login.js
@@ -1,27 +1,14 @@
-import React from "react";
-import {useState}from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 function Login()
  {
-  let[loginData,setLoginData]=useState({email:"",password:""})  
-  const checkLogin=()=>{
-    if(loginData.email===""&&loginData.password==="")
-    { 
-      alert("success")
+ 
   
     return(
       <>
       
-      <label for="email"> email:</label>
-      <input type="email" id="email" name="email"defaultValue={loginData.email} onKeyUp={(e)=>setLoginData({...loginData,email:e.target.value})}></input><br></br>
-      <label for="password"> password:</label>
-      <input type="password" id="password" name="password" defaultValue={loginData.password} onKeyUp={(e)=>setLoginData({...loginData,password:e.target.value})}></input><br></br>
-      <button type="submit" onClick={()=>checkLogin()}>Login</button>
 
       </>
 
-    )}
+    )
   }
-}
 export default Login
