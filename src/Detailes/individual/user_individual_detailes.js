5118ed30078949e29f918d7a5896f6af2daaab50
muruges-wari <gayathrikarthi102@gmail.com>
2023-12-27 12:09:45 +0530

    mini project

--- src/Detailes/individual/user_individual_detailes.js
+++ src/Detailes/individual/user_individual_detailes.js
@@ -11,14 +11,14 @@ function UserIndividualDetailes() {
   const individual=useSelector((state)=>state.userdetail.individual)
   const status=useSelector((state)=>state.userdetail.status)
   const {userId}=useParams()
-  console.log(individual[0].name)
+  console.log(individual)
   const dispatch=useDispatch()
   useEffect(() => {
     axios.get(`https://agaram.academy/api/shh/index.php?request=get_group_details&group_id=1`)
       .then(res => dispatch(individualData(res.data.data.members
         )))
   }, [])
-  // console.log(individual[0])
+ 
 
   const razorPay=()=>{
     dispatch(statusData(true))
@@ -28,14 +28,21 @@ function UserIndividualDetailes() {
   return (
     <>
     <div class="full">
+      
       <Card class="card text-white bg-dark mb-3" style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt7C1jVOcBY3LpyT3GJcdQHAYyBuW6Js6h3w&usqp=CAU" />
       <Card.Body class="body">
         
           <>
-          <h3>{individual[0].name}</h3>
-          <h3>{individual[0].email}</h3>
-          <h3>{individual[0].phone}</h3>
+          {individual.map((indi)=>
+            <>
+            {indi.id==2?<>
+            <h3>{indi.name}</h3>
+            <h3>{indi.email}</h3>
+            <h3>{indi.phone}</h3>
+            </>:""}
+            </>
+          )}
           <h3>1000</h3>
           Payment Status: {status?<span class="button">Paid</span>:<span>Pending</span>}<br/>
           <button onClick={()=>razorPay()}>Pay</button>
--- src/redux/create_slice.js
+++ src/redux/create_slice.js
@@ -4,7 +4,6 @@ export const CounterSlice = createSlice({
   name: 'userdetail',
   initialState: {
     individual:[{
-      name:"",
     }],
     admin:{
       members:[]
